be91896dbbf98719dd18a4342e81d0ef7d1b09d8
sbx_user1051 <sbx_user1051@169.254.9.125>
2025-11-14 14:45:54 +0000

    Fri Nov 14 14:45:54 UTC 2025

--- lib/solutions/SUM/sum_solution.py
+++ lib/solutions/SUM/sum_solution.py
@@ -3,5 +3,7 @@ class SumSolution:
     
     def compute(self, x, y):
         if not (0 <= x <= 100 or 0 <= y <= 100):
-            raise ValueError('invalid value')
+            return "incorrect argument x and y both need to be between 0 and 100"
+            # raise ValueError('invalid value')
         return x + y
+
