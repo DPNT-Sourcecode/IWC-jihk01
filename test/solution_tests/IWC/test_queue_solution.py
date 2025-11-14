56163617c89be3dde8845ed223910872ed59e838
sbx_user1051 <sbx_user1051@169.254.9.125>
2025-11-14 17:01:20 +0000

    Fri Nov 14 17:01:20 UTC 2025

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 import unittest
+from unittest.mock import patch
 
 from .utils import call_dequeue, call_enqueue, call_size, iso_ts, run_queue
 
@@ -23,34 +24,36 @@ def test_task_already_in_queue() -> None:
         call_enqueue("companies_house", 2, iso_ts(delta_minutes=0)).expect(3),
     ])
 
+class TestQueue(unittest.TestCase):
+    def setUp(self):
+        self.queue = Queue()
 
-def test_maintain_schedule_order() -> None:
-    queue = Queue()
+        self.t1 = TaskSubmission(
+            provider="companies_house",
+            user_id=3,
+            timestamp=iso_ts(delta_minutes=0),
+            metadata={"priority": Priority.NORMAL}
+        )
 
-    t1 = TaskSubmission(
-        provider="companies_house",
-        user_id=3,
-        timestamp=iso_ts(delta_minutes=0),
-        metadata={"priority": Priority.NORMAL}
-    )
+        self.t2 = TaskSubmission(
+            provider="companies",
+            user_id=3,
+            timestamp=iso_ts(delta_minutes=0),
+            metadata={"priority": Priority.NORMAL}
+        )
 
-    t2 = TaskSubmission(
-        provider="companies",
-        user_id=3,
-        timestamp=iso_ts(delta_minutes=0),
-        metadata={"priority": Priority.NORMAL}
-    )
+    def test_maintain_schedule_order(self) -> None:
+        self.queue.enqueue(self.t1)
+        self.queue.enqueue(self.t2)
 
-    queue.enqueue(t1)
-    queue.enqueue(t2)
+        # this is a hacky test but here I am going to simulate the order being shuffled
+        # as happens in dequeue due to the 3 rule
+        queue_shuffled = [self.queue._queue[1], self.queue._queue[0]]
+        with patch.object(self.queue, "_queue", new=queue_shuffled):
+            a = self.queue.dequeue()
+            b = self.t1
+            self.assertEqual(self.queue.dequeue(), self.t1)   # should be t1
 
-    # this is a hacky test but here I am going to simulate the order being shuffled
-    # as happens in dequeue due to the 3 rule
-    queue_shuffled = [queue._queue[1], queue._queue[0]]
-    from unittest.mock import patch
-    with patch.object(queue, "_queue", new=queue_shuffled):
-        assertEqual(queue.dequeue(), t1)   # should be t1
-        a=1
 
 
 
