2660e8e2cdd61997d66c9c81163a04ab8eeb2461
sbx_user1051 <sbx_user1051@169.254.9.125>
2025-11-14 15:52:20 +0000

    Fri Nov 14 15:52:20 UTC 2025

--- test/solution_tests/IWC/test_queue_solution.py
+++ test/solution_tests/IWC/test_queue_solution.py
@@ -16,7 +16,8 @@ def test_enqueue_size_dequeue_flow() -> None:
 def test_task_already_in_queue() -> None:
     run_queue([
         call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(1),
-        call_size().expect(1),
-        call_dequeue().expect("companies_house", 1),
-        
+        call_enqueue("companies", 1, iso_ts(delta_minutes=0)).expect(2),
+        call_enqueue("companies_house", 1, iso_ts(delta_minutes=0)).expect(2),
+        call_enqueue("companies_house", 2, iso_ts(delta_minutes=0)).expect(3),
+
     ])
